ffa52e4c3df4c02428a686d069a8d201ee76136e
kgaugelo943 <kgaugelopheeha94@gmail.com>
2025-04-30 17:36:08 +0200

    Submit assignment

---
+++ script.js
@@ -0,0 +1,18 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", () => {
+    const orderButton = document.getElementById("order-button");
+    
+    orderButton.addEventListener("click", () => {
+      alert("Thanks for your interest! Online ordering is coming soon.");
+    });
+  });
+
+   // Change heading text and style
+   heading.textContent = "SimplyKgau: Healthy Treats for Healthy Kids!";
+   heading.style.backgroundColor = "#dff0d8";
+   heading.style.color = "#2e7d32";
+   heading.style.padding = "15px";
+   heading.style.borderRadius = "10px";
+   heading.style.textAlign = "center";
+  
